ffe91b68bace5b0fd5e35c354c2f72b7e16169ea
Aadhithya R P <96223165+Aadhithya-R-P@users.noreply.github.com>
2023-12-20 12:09:47 +0530

    Create searchInSortedRotatedArray.cpp

---
+++ DSA_SHEET/Binary_Search/searchInSortedRotatedArray.cpp
@@ -0,0 +1,53 @@
+class Solution{
+    public:
+    int search(int A[], int l, int h, int key){
+        // l: The starting index
+        // h: The ending index, you have to search the key in this range
+        
+        //complete the function here
+        int m;
+        while(l<=h)
+        {
+            m = l + (h-l)/2;
+            if(A[m] == key)
+                return m;
+            else if(A[m] > key)
+            {
+                if(A[m] >= A[l])
+                {
+                    if(A[l] > key)
+                    {
+                        l = m+1;
+                    }
+                    else
+                    {
+                        h = m-1;
+                    }
+                }
+                else
+                {
+                   h = m-1;
+                }
+            }
+            else
+            {
+                if(A[l] > key)
+                {
+                    l = m+1;
+                }
+                else
+                {
+                    if(A[l] > A[m])
+                    {
+                        h = m-1;
+                    }
+                    else
+                    {
+                        l = m+1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
